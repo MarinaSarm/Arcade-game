280e0f7f686ebbaa4f450f984eb32fba11df0a98
MarinaSarm <marsarman@yandex.ru>
2018-06-13 13:40:24 +0200

    write update function for Enemies

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+    this.x = this.x + this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -41,7 +41,7 @@ Player.prototype.update = function() {
     // x = (this.x + stepX);
     // y = (this.y + stepY);
 }
-// Now instantiate your objects.
+// Now instantiate your objects
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
